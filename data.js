3b3484d7174b34ffd9c617c9042e53acefd4399d
Guille <91394918+GuilleCap@users.noreply.github.com>
2021-12-19 16:21:56 -0500

    finish to add functions and valiables

--- data.js
+++ data.js
@@ -7,26 +7,26 @@ let songs = [
     },
     {
         name: 'song 2',
-        path: 'musics/Song 2.mp3',
+        path: 'musics/musics/Song 2.mp3',
         artist: 'artist 2',
-        cover: 'images/cover 2.png'
+        cover: 'images/images/cover 2.png'
     },
     {
         name: 'song 3',
-        path: 'musics/Song 3.mp3',
+        path: 'musics/musics/Song 3.mp3',
         artist: 'artist 3',
-        cover: 'images/cover 3.png'
+        cover: 'images/images/cover 3.png'
     },
     {
         name: 'song 4',
-        path: 'musics/Song 4.mp3',
+        path: 'musics/musics/Song 4.mp3',
         artist: 'artist 4',
-        cover: 'images/cover 4.png'
+        cover: 'images/images/cover 4.png'
     },
     {
         name: 'song 5',
-        path: 'musics/Song 5.mp3',
+        path: 'musics/musics/Song 5.mp3',
         artist: 'artist 5',
-        cover: 'images/cover 5.png'
+        cover: 'images/images/cover 5.png'
     },
 ]
--- script.js
+++ script.js
@@ -13,6 +13,13 @@ const forwardBtn = document.querySelector('.forward-btn');
 const backwardBtn = document.querySelector('.backward-btn');
 
 playBtn.addEventListener('click', () => {
+    if(playBtn.className.includes('pause')){
+        music.play();
+    } else {
+        music.pause();
+    }
+    
+    
     playBtn.classList.toggle('pause');
     disk.classList.toggle('play');
 })
@@ -32,13 +39,99 @@ const setMusic = (i) => {
     currentTime.innerHTML = '00:00';
     setTimeout(() => {
         seekBar.max = music.duration;
-        console.log(music.duration);
+        musicDuration.innerHTML = formatTime(music.duration);
     }, 300);
     
 
 }
 
 setMusic(0);
+
+// formatting time in min and seconds format
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if(min < 10) {
+        min = `0${min}`;
+    }
+
+    let sec = Math.floor(time % 60)
+    if(sec < 10) {
+        sec = `0${sec}`;
+    }
+    return `${min} : ${sec}`;  
+}
+
+// seek bar
+
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    currentTime.innerHTML = formatTime(music.currentTime);
+    if(Math.floor(music.currentTime) == Math.floor(seekBar.max)){
+        forwardBtn.click();
+    } 
+}, 500)
+
+seekBar.addEventListener('change', () => {
+    music.currentTime = seekBar.value;
+})
+
+const playMusic = () => {
+    music.play();
+    playBtn.classList.remove('pause');
+    disk.classList.add('play');
+}
+
+
+
+
+// forward and backward button
+
+ forwardBtn.addEventListener('click', () => {
+     if(currentMusic >= songs.length - 1){
+         currentMusic = 0;
+    } else {
+        currentMusic++;
+    }
+    setMusic(currentMusic);
+    playMusic();
+ })
+
+
+ backwardBtn.addEventListener('click', () => {
+    if(currentMusic <= 0){
+        currentMusic = song.length - 1;
+   } else {
+       currentMusic--;
+   }
+   setMusic(currentMusic);
+   playMusic(); 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
