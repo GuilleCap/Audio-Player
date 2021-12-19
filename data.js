48d398786b48b7ac85427adc57f1be63437c2474
Guille <91394918+GuilleCap@users.noreply.github.com>
2021-12-19 17:15:17 -0500

    fixing some details

--- data.js
+++ data.js
@@ -1,32 +1,98 @@
 let songs = [
     {
-        name: 'song 1',
-        path: 'musics/musics/Song 1.mp3',
+        name: 'banda',
+        path: 'musics/musics/banda.mp3',
         artist: 'artist 1',
-        cover: 'images/images/cover 1.png'
+        cover: 'images/images/decadentes.png'
     },
     {
-        name: 'song 2',
-        path: 'musics/musics/Song 2.mp3',
+        name: 'bifurcada',
+        path: 'musics/musics/bifurcada.mp3',
         artist: 'artist 2',
-        cover: 'images/images/cover 2.png'
+        cover: 'images/images/memphis.png'
     },
     {
-        name: 'song 3',
-        path: 'musics/musics/Song 3.mp3',
+        name: 'campo',
+        path: 'musics/musics/campo.mp3',
         artist: 'artist 3',
-        cover: 'images/images/cover 3.png'
+        cover: 'images/images/campo.png'
     },
     {
-        name: 'song 4',
-        path: 'musics/musics/Song 4.mp3',
+        name: 'carnaval',
+        path: 'musics/musics/carnaval.mp3',
         artist: 'artist 4',
-        cover: 'images/images/cover 4.png'
+        cover: 'images/images/fabulosos.png'
     },
     {
-        name: 'song 5',
-        path: 'musics/musics/Song 5.mp3',
+        name: 'costumbres',
+        path: 'musics/musics/costumbres.mp3',
         artist: 'artist 5',
-        cover: 'images/images/cover 5.png'
+        cover: 'images/images/abuelos.png'
     },
+    {
+        name: 'enanitos',
+        path: 'musics/musics/enanitos.mp3',
+        artist: 'artist 6',
+        cover: 'images/images/enanos.png'
+    },
+    {
+        name: 'feliz',
+        path: 'musics/musics/feliz.mp3',
+        artist: 'artist 7',
+        cover: 'images/images/ntvg.png'
+    },
+    {
+        name: 'marias',
+        path: 'musics/musics/marias.mp3',
+        artist: 'artist 8',
+        cover: 'images/images/marias.png'
+    },
+    {
+        name: 'memphis',
+        path: 'musics/musics/memphis.mp3',
+        artist: 'artist 9',
+        cover: 'images/images/memphis.png'
+    },
+    {
+        name: 'muera',
+        path: 'musics/musics/muera.mp3',
+        artist: 'artist 10',
+        cover: 'images/images/muera.png'
+    },
+    {
+        name: 'nos',
+        path: 'musics/musics/nos.mp3',
+        artist: 'artist 11',
+        cover: 'images/images/once.png'
+    },
+    {
+        name: 'poeta',
+        path: 'musics/musics/poeta.mp3',
+        artist: 'artist 12',
+        cover: 'images/images/trampa.png'
+    },
+    {
+        name: 'tv',
+        path: 'musics/musics/tv.mp3',
+        artist: 'artist 13',
+        cover: 'images/images/trotsky.png'
+    },
+    {
+        name: 'vencedores',
+        path: 'musics/musics/vencedores.mp3',
+        artist: 'artist 14',
+        cover: 'images/images/redondos.png'
+    },
+    {
+        name: 'verano',
+        path: 'musics/musics/verano.mp3',
+        artist: 'artist 15',
+        cover: 'images/images/ratones.png'
+    },
+    {
+        name: 'viejo',
+        path: 'musics/musics/viejo.mp3',
+        artist: 'artist 16',
+        cover: 'images/images/puerca.png'
+    }
 ]
